dcd2192efbdcbfd9f8567648250e74d997665bd4
merlin-777 <157508874+merlin-777@users.noreply.github.com>
2024-02-09 14:51:08 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function encode() {
+    document.getElementById("translation-content").innerHTML = "Hello World";
+}
+
+function decode() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function copyText() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
